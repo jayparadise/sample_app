273c2532ea2da26a13c21d0f1f934db57de9c06d
Jason Paradise <jasonrparadise@gmail.com>
2015-01-07 19:11:39 +0000

    exercise: changed base title

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   #   get 'products/:id' => 'catalog#view'
   get 'static_pages/help'
   get 'static_pages/about'
-  
+
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
